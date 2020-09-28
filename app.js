036920b9cc102b86436fbad35c27cb0a05c90278
tushx3 <tkushwaha70@gmail.com>
2020-09-28 17:22:49 +0530

    finished crud operations in all new pages functionality

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const  eDate  = require(__dirname + "/day.js");
 const mongoose = require("mongoose");
 const app = express();
+const _ = require("lodash");
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useUnifiedTopology: 1,useNewUrlParser: 1})
-
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useUnifiedTopology: 1,useNewUrlParser: 1,useFindAndModify: false})
+// item schema here
 const itemSchema =  {
     name: String
 }
+// list schema here
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+};
 
 const Item = mongoose.model("item",itemSchema);
 
-const WorkItem = mongoose.model("workitem",itemSchema);
-
-const newitem0 = new WorkItem({
-    name: "Hello"
-});
-newitem0.save();
-
+const List = mongoose.model("list",listSchema);
+// ----------------------------------------------default entries for every new page--------------------------------------------------------------
 const newitem1 = new Item({
     name: "Hello"
 });
@@ -28,60 +28,118 @@ const newitem2 = new Item({
 });
 
 const newitem3 = new Item({
-    name: "below"
+    name: "delete by checking the checkbox"
 });
 
-// Item.insertMany([newitem1,newitem2,newitem3]);
+const defaultitems = [ newitem1, newitem2, newitem3];
+// ----------------------------------------------------------------------------------------------------------------------------------------------
+
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true })); 
+
 app.use(express.static("public/"));
 
+// ----------------------------------------------------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------get for root route--------------------------------------------------------------
+
 app.get("/", (req,res)=>{
-    var day = eDate.getDate();
     Item.find({},(err,listitems)=>{
         if(listitems===0){
             Item.insertMany([newitem1,newitem2,newitem3]);
             res.render("/");
         }
         else{
-            res.render("list", {eday: day,enewlist: listitems});
+            res.render("list", {eday: "Today",enewlist: listitems});
         }
     })
 });
 
-app.get("/work", (req,res)=>{
-    WorkItem.find({},(err,listitems)=>{
-        if(listitems===0){
-            WorkItem.insertMany([newitem1,newitem2,newitem3]);
-            res.render("/");
-        }
-        else{
-            res.render("list", {eday: "WorkList",enewlist: listitems});
-        }
-       })
+// ----------------------------------------------get for every new page--------------------------------------------------------------
+
+app.get("/:anything", (req,res)=>{
+        const customListName = _.capitalize(req.params.anything);
+      List.findOne({name:customListName},(err,result)=>{
+        if(!err){
+            if(!result){
+                const list = new List({
+                    name: customListName,
+                    items: defaultitems
+                });
+        
+                list.save();
+                res.redirect("/"+customListName);
+            }
+            else{
+                res.render("list", {eday: customListName,enewlist: result.items});
+            }
+             
+          }
+          else{
+            console.log(err);
+          }
+      })
+        
 })
 
-
+// ----------------------------------------------post for every page--------------------------------------------------------------
 
 app.post("/", (req,res)=>{
 
-    const enewitem = req.body.etx;
+    const list_title = req.body.subName;
+    const list_newItem = req.body.newListInput;
 
-    if(req.body.subName==="WorkList"){
-        const item2add2 = new WorkItem({
-            name: enewitem
+
+        const itemadd = new Item({
+            name: list_newItem
         });
-        item2add2.save();
-        res.redirect("/work");
+
+        if(list_title==="Today"){
+            itemadd.save();
+            res.redirect("/");
+        }
+        else{
+            List.findOne({name: list_title}, (err,result)=>{
+               if(!err){
+                   if(result){
+                        result.items.push(itemadd);
+                        result.save();
+                        res.redirect("/"+list_title);
+                   }
+                  else{
+                      console.log("wrong list");
+                  }
+               }
+               else{
+                   console.log(err);
+               }
+            });
+        }
+});
+
+// ----------------------------------------------delete section--------------------------------------------------------------
+
+
+app.post("/delete", (req,res)=>{
+    const itemToDelete = req.body.itemTobeDeleted;
+    const listToDeleteFrom = req.body.hidden_listName;
+
+    if(listToDeleteFrom==="Today"){
+        Item.findByIdAndRemove(itemToDelete, (err)=>{
+            if(!err){
+                console.log("Successfully deleted");
+                res.redirect("/");
+            }
+        })
     }
-    else {
-        const item2add = new Item({
-            name: enewitem
-        });
-        item2add.save();
-        res.redirect("/");
+    else{
+        List.findOneAndUpdate({name: listToDeleteFrom}, {$pull: {items: {_id: itemToDelete}}}, (err,foundList)=>{
+            if(!err){
+                res.redirect("/"+listToDeleteFrom);
+            }
+        })
     }
 });
 
